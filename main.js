1fb7d0b9dc03096aeccb567d375cf7adba32d40c
Amrahova <10programmer01@gmail.com>
2023-12-08 18:59:59 +0400

    make Counter

--- main.js
+++ main.js
@@ -1,22 +1,62 @@
-let decBtn = document.querySelector("#decBtn");
-let resetBtn = document.querySelector("#resetBtn");
-let incBtn = document.querySelector("#incBtn");
+// let decBtn = document.querySelector("#decBtn");
+// let resetBtn = document.querySelector("#resetBtn");
+// let incBtn = document.querySelector("#incBtn");
+// let changeNum = document.querySelector(".changeNum");
+// let number=0;
+//First compound
+// decBtn.addEventListener("click", function () {
+//     number--;
+//     changeNum.innerHTML = number;
+//     console.log("- ", newNum);
+// });
+
+// resetBtn.addEventListener("click", function () {
+//     changeNum.innerHTML = number = 0;
+//     console.log("reset ", number);
+// });
+
+// incBtn.addEventListener("click", function () {
+//     number++;
+//     changeNum.innerHTML = number;
+//     console.log("+", newNum);
+// })
+
+//Secound Compound
+
 let changeNum = document.querySelector(".changeNum");
-let number=0;
+let btns = document.querySelectorAll(".btn");
+let count = 0;
 
-decBtn.addEventListener("click", function () {
-    let newNum = number--;
-    changeNum.innerHTML = `${newNum}`;
-    console.log("- ", newNum);
+btns.forEach((btn) => {
+    // console.log(btn);
+    btn.addEventListener("click", function (e) {
+        let classShow = e.currentTarget.classList;
+        // console.log(classShow);
+        if (classShow.contains("decBtn")) {
+            count--;
+            
+        }
+        else if (classShow.contains("incBtn")) {
+            count++;
+            
+        }
+        else {
+            count = 0;
+        }
+
+        if(count>0){
+            changeNum.style.color="green";
+        }
+        else if(count<0){
+            changeNum.style.color="red";
+        }
+        else{
+            changeNum.style.color="black";
+        }
+        changeNum.textContent=count;
+
+    });
+
+
 });
 
-resetBtn.addEventListener("click", function () {
-    changeNum.innerHTML = `${number = 0}`;
-    console.log("reset ", number);
-});
-
-incBtn.addEventListener("click", function () {
-    newNum=number++;
-    changeNum.innerHTML = `${newNum}`
-    console.log("+", newNum);
-})
